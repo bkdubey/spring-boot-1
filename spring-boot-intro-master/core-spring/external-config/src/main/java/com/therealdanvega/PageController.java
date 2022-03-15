d56eecda388b726b676f17d3102843e33b161aa4
dubeybk15 <dubeybk15@gmail.com>
2022-03-15 20:41:23 +0530

    adding missing props & can read from program argument as well .

--- spring-boot-intro-master/core-spring/external-config/src/main/java/com/therealdanvega/PageController.java
+++ spring-boot-intro-master/core-spring/external-config/src/main/java/com/therealdanvega/PageController.java
@@ -18,4 +18,13 @@ public class PageController {
         return mySecret;
     }
 
+    // need to pass this in program argument ==>  --read.from.commandLine="read from command line"
+    @Value("${read.from.commandLine}")
+    private String readFromCommandLine;
+
+    @RequestMapping("/readFromCommandLine")
+    public String readFromCommandLine(){
+        return readFromCommandLine;
+    }
+
 }
