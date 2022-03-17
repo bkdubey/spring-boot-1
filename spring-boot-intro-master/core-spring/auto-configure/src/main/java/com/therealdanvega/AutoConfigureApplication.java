c5a542103be1209007bb6b4ec1bb1f8326c17ffd
dubeybk15 <dubeybk15@gmail.com>
2022-03-17 19:53:01 +0530

    we can do exclude class if we do not want that class to be auto configure .

--- spring-boot-intro-master/core-spring/auto-configure/src/main/java/com/therealdanvega/AutoConfigureApplication.java
+++ spring-boot-intro-master/core-spring/auto-configure/src/main/java/com/therealdanvega/AutoConfigureApplication.java
@@ -3,11 +3,12 @@ package com.therealdanvega;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {ThymeleafAutoConfiguration.class})
 @ComponentScan
 public class AutoConfigureApplication {
 
